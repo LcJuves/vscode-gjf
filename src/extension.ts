5efc5bf91c320dc681c6e196a3fd615ab3556492
Liangcheng Juves <liangchengj@outlook.com>
2021-06-07 00:50:12 +0800

    Use Google Java Format to format the selected text

--- src/extension.ts
+++ src/extension.ts
@@ -92,15 +92,47 @@ function _vscodeGJFFixImportsOnly(textEditor: vscode.TextEditor, edit: vscode.Te
 	}
 }
 
+function _vscodeGJFFormatWithSelection(textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit) {
+	let gjfConfigOptions = getGJFConfigOptions();
+	try {
+		let _cmdOptions: string = '--skip-removing-unused-imports';
+		if (gjfConfigOptions.useAOSPStyle) { _cmdOptions += ' --aosp'; }
+		let _startPosition = textEditor.selection.start;
+		let _length = textEditor.document.getText(textEditor.selection).length;
+		_cmdOptions += ` --line=${_startPosition.line} --offset=${_startPosition.character}`;
+		_cmdOptions += ` --length=${_length}`;
+		_cmdOptions += ' -';
+
+		let _input = textEditor.document.getText();
+		let _stdout: string = _execGJFJarSync(gjfConfigOptions, _input, _cmdOptions);
+
+		let _endPosition = textEditor.selection.end;
+		let _beforeOffset = textEditor.document.offsetAt(_endPosition);
+
+		edit.replace(rangeOfDocument(textEditor.document), _stdout);
+
+		let _affectedLength = _stdout.length - _input.length;
+		textEditor.selection = new vscode.Selection(
+			_startPosition,
+			textEditor.document.positionAt(_beforeOffset + _affectedLength)
+		);
+	} catch (e) {
+		_resolveExecJarPathError(gjfConfigOptions, e);
+	}
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	let _documentRangeFormattingEditProvider = vscode.languages.registerDocumentRangeFormattingEditProvider(
-		gjfDocumentFilter,
-		new GJFDocumentRangeFormattingEditProvider()
+	context.subscriptions.push(
+		vscode.languages.registerDocumentRangeFormattingEditProvider(
+			gjfDocumentFilter,
+			new GJFDocumentRangeFormattingEditProvider()
+		),
+		vscode.commands.registerTextEditorCommand('vscode-gjf.fix-imports-only', _vscodeGJFFixImportsOnly),
+		vscode.commands.registerTextEditorCommand('vscode-gjf.format-with-selection', _vscodeGJFFormatWithSelection)
 	);
-	let _command = vscode.commands.registerTextEditorCommand('vscode-gjf.fix-imports-only', _vscodeGJFFixImportsOnly);
-	context.subscriptions.push(_documentRangeFormattingEditProvider, _command);
+
 }
 
 // this method is called when your extension is deactivated
@@ -111,7 +143,7 @@ export function deactivate() { }
  *
  * When a section-identifier is provided only that part of the configuration
  * is returned. Dots in the section-identifier are interpreted as child-access,
- * like `{ myExt: { setting: { doIt: true }}}` and `getConfiguration('myExt.setting').get('doIt') === true`.
+ * like `{ myExt: { setting: { doIt: true } } } ` and `getConfiguration('myExt.setting').get('doIt') === true`.
  *
  * When a scope is provided configuration confined to that scope is returned. Scope can be a resource or a language identifier or both.
  *
